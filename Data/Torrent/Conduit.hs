643f84bd62e716a0a6a7bd6f7ca853def746da2f
Markus Barenhoff <alios@alios.org>
2013-03-08 09:20:30 +0100

    added MkTorrent

--- Data/Torrent/Conduit.hs
+++ Data/Torrent/Conduit.hs
@@ -1,25 +1,42 @@
-{-# LANGUAGE FlexibleContexts, ImpredicativeTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Data.Torrent.Conduit 
        ( sinkBencoded
        , sinkBdecoded
        , conduitBencoded
        , conduitBdecoded
+       , mkTorrent
+       , mkTorrent'
+       , defaultChunkSize
+       , hashChunks
        ) where
 
+import qualified Prelude as P
 import Prelude hiding (FilePath, head)
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Resource
-import Data.Torrent.Types
+import Filesystem.Path.CurrentOS
+import qualified Data.List as List (map)
+
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Resource (MonadActive, liftResourceT)
+
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import Data.Maybe (fromJust)
+
 import Data.Conduit
-import Data.Conduit.List as CL
-import Data.Conduit.Lazy
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.Filesystem as CFS
 import Data.Conduit.Attoparsec
-import Data.Conduit.Filesystem
-import Filesystem.Path
-import qualified Codec.Binary.UTF8.Generic as UTF8
+import Crypto.Conduit (sinkHash)
+import Crypto.Hash.SHA1
+import qualified Crypto.Classes as Crypto
+import System.Posix.Files (isDirectory, getFileStatus)
+
+
+
+import Data.Torrent.Types
 
 sinkBencoded :: (MonadThrow m) => Consumer ByteString m BEncodedT
 sinkBencoded = sinkParser parseBencoded
@@ -34,68 +51,73 @@ sinkBdecoded =
 conduitBdecoded :: (MonadThrow m) => Conduit BEncodedT m ByteString
 conduitBdecoded = awaitForever $ yield . beEncodeByteString 
 
+defaultChunkSize :: Integer
+defaultChunkSize = 2 ^ 18
 
+mkTorrent :: (MonadIO m, MonadUnsafeIO m, MonadThrow m, MonadBaseControl IO m) =>
+             FilePath -> [String] -> Integer -> m (Maybe BEncodedT)  
+mkTorrent fp ts cSize = 
+  runResourceT $ liftResourceT $ 
+  mkTorrent' fp ts cSize $$ await
 
-
-{--
-
-emptyTM :: TorrentMonad
-emptyTM = (BDict [], [])
-
-
---foldTorrent :: (MonadIO m ) => Consumer FilePath m (TorrentMonad m)
---foldTorrent = foldM encodeTorrent emptyTM
-
-encodeTorrent :: TorrentMonad -> FilePath -> m TorrentMonad
-encodeTorrent torIn@(BDict [], []) fp = do 
-  tor' <- addTracker torIn
-  encodeTorrent (fst tor', []) fp
-encodeTorrent (dict, fs) fp = do
-  --          fileSrc <-  lazyConsume $ sourceFile fp
-  return (dict,  fs)
-
-addTracker :: TorrentMonad -> m TorrentMonad
-addTracker (tor, fs) = do
-  ann <- trackerSource $$ head 
-  case (ann) of
-    Nothing -> error $ "no tracker specified in " ++ show trackers
-    Just ann' -> 
-      return $ (BDict [ (mkBString "announce", mkBString ann') ], fs)
-
-
-trackers :: [String]
-trackers = []
-trackerSource = sourceList trackers
-
-
---}
-
-
-
-mkTorrent :: (MonadActive m, MonadBaseControl IO m, MonadResource m) => 
-             FilePath -> [String] -> Int -> Producer m BEncodedT
-mkTorrent fpBase tracker cSize = do
-  
-  -- add announce tracker to dict
-  let trackerSource = sourceList tracker
-  ann' <-  trackerSource $$ head
-  let ann = maybe (error "no tracker specified") id ann'
+mkTorrent' :: (MonadActive m, MonadBaseControl IO m, MonadResource m) => 
+             FilePath -> [String] -> Integer -> Producer m BEncodedT
+mkTorrent' fpBase tracker cSize = do  
+  -- file or directory?
+  isDir <- fmap isDirectory $ liftIO $ 
+           getFileStatus $ encodeString fpBase
   
   -- traverse files
---  let fileTravers = traverse False fpBase $$ foldFilePaths
---  let ffold = foldM  (BS.empty, [])
+  (bs', fs) <- if (isDir) 
+               then liftIO $ runResourceT $ 
+                    CFS.traverse False fpBase $$ foldFilePaths fpBase
+               else do
+                 bs' <- fmap BL.fromChunks $ CFS.sourceFile fpBase $$ CL.consume
+                 return (bs', [(fpBase, toInteger . BL.length $ bs')])
   
-  --x <- fileTravers 
+  -- hash pieces 
+  pieces' <- (CB.sourceLbs $ bs') $= hashChunks (fromInteger cSize) $$ CL.consume
+  let pieces = BS.concat $ List.map Crypto.encode pieces'
   
-  yield $ BDict [(mkBString "announce", mkBString ann)]
+  -- add announce tracker to dict
+  ann' <- CL.sourceList tracker $$ CL.head
+  let ann = maybe (error "no tracker specified") id ann'
+  
+  let info = BDict $ [
+        (mkBString "name", mkBString . encodeString . filename $ fpBase),
+        (mkBString "piece length", BInteger . toInteger $ cSize),
+        (mkBString "pieces", BString $ pieces)
+        ] ++ if ((length fs) == 1) 
+             then [(mkBString "length", BInteger . toInteger . BL.length $ bs')]
+             else [(mkBString "files", 
+                    BList [ BDict [(mkBString "length", BInteger $ toInteger l), 
+                                   (mkBString "path", mkBString . encodeString $ fn )] 
+                          | (fn,l) <- fs])]
+  
+  yield $ BDict [
+    (mkBString "announce", mkBString ann),
+    (mkBString "info", info)
+    ]
+
 
 
 foldFilePaths :: (Monad m, MonadActive m, MonadBaseControl IO m, MonadResource m) => 
-                 Consumer FilePath m (ByteString, [(FilePath, Int)])
-foldFilePaths = foldM f (BS.empty, []) 
+                 FilePath -> Consumer FilePath m (BL.ByteString, [(FilePath, Integer)])
+foldFilePaths fpBase = CL.foldM f (BL.empty, []) 
   where f :: (Monad m, MonadActive m, MonadBaseControl IO m, MonadResource m) => 
-             (ByteString, [(FilePath, Int)]) -> FilePath -> m (ByteString, [(FilePath, Int)])
+             (BL.ByteString, [(FilePath, Integer)]) -> FilePath -> m (BL.ByteString, [(FilePath, Integer)])
         f (bs, fs) fp = do
-          bs' <- fmap BS.concat (lazyConsume $ sourceFile fp)
-          return (bs `BS.append` bs' , fs ++ [(fp, BS.length bs')])
+          bs' <- fmap BS.concat (CFS.sourceFile fp $$ CL.consume)
+          return (bs `BL.append` BL.fromChunks [bs'] , fs ++ [(fromJust $ stripPrefix fpBase fp, toInteger $ BS.length bs')])
 
+hashChunks :: (Monad m) => Int -> Conduit ByteString m SHA1
+hashChunks cSize = do
+  chunk <- CB.take cSize
+  h <- CB.sourceLbs chunk $$ sinkHash
+  yield h
+  next <- CL.peek
+  case next of
+    Nothing -> return ()
+    Just _ -> hashChunks cSize
+
+  
